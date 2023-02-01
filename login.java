ad67d52b12e25ed3506178c2a75b6a3de65ab480
AnkitaDamkondwar <99202357+AnkitaDamkondwar@users.noreply.github.com>
2023-02-01 16:50:40 +0530

    Update login.java

--- login.java
+++ login.java
@@ -2,10 +2,9 @@ public class login {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+     
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Hello,World!");
    }
 }
 
